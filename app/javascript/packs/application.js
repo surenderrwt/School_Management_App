ad3a2fb2905457ef6f1d73b7fbd5ea2d99766de6
Surender Rawat <rawat.surender87@gmail.com>
2020-06-11 22:58:29 +0530

    fix ' issue in Applicaiton.css -- School MGM App

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import '../stylesheets/application'
-import 'materialize-css/dist/js/materialize'
+
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -17,3 +16,6 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import '../stylesheets/application'
+import 'materialize-css/dist/js/materialize'
